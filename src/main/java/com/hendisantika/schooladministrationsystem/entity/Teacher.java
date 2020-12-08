e927c1c5067318bcb863ee14885c4e1665eb2735
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-08 15:19:44 +0700

    #16 - Add Teacher Entity

---
+++ src/main/java/com/hendisantika/schooladministrationsystem/entity/Teacher.java
@@ -0,0 +1,78 @@
+package com.hendisantika.schooladministrationsystem.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : school-administration-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/12/20
+ * Time: 15.04
+ */
+@Entity
+@Table(name = "teachers")
+@Data
+@NoArgsConstructor
+public class Teacher {
+
+    /**
+     * Id field [GENERATED AUTOMATICALLY].
+     */
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * User object to teacher. This property connects the teacher to
+     * the account.
+     */
+    @ManyToOne
+    @JoinColumn(name="teacher_id")
+    private User teacher;
+
+    /**
+     * Teacher e-mail.
+     */
+    @Column(name = "email", length = 64)
+    private String email;
+
+    /**
+     * Teacher phone number.
+     */
+    @Column(name = "phone", nullable = false, length = 24)
+    private String phone;
+
+    /**
+     * Empty constructor.
+     */
+    public Teacher() {}
+
+    /**
+     *  Constructor to make a new instance.
+     *
+     * @param teacher User object to teacher.
+     * @param email Teacher e-mail.
+     * @param phone Teacher phone number.
+     */
+    public Teacher(User teacher, String email, String phone) {
+        this.teacher = teacher;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    /**
+     * Courses that the teacher teaching.
+     */
+    @JsonIgnore
+    @OneToMany(mappedBy = "teacher")
+    private List<Course> courses = new ArrayList<>();
+}
