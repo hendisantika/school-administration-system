ab691b1167c092a844c8983c1bb16ed7eeb4f3c8
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-08 15:08:43 +0700

    #6 - Add Gender Enum Entity

---
+++ src/main/java/com/hendisantika/schooladministrationsystem/entity/Gender.java
@@ -0,0 +1,16 @@
+package com.hendisantika.schooladministrationsystem.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : school-administration-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/12/20
+ * Time: 15.08
+ */
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
