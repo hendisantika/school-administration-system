5b343f66aabd434573d1bed005f94db93b805cec
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-08 14:53:23 +0700

    #1 - first commit

---
+++ src/main/java/com/hendisantika/schooladministrationsystem/SchoolAdministrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.schooladministrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolAdministrationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolAdministrationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/schooladministrationsystem/SchoolAdministrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.schooladministrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolAdministrationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
