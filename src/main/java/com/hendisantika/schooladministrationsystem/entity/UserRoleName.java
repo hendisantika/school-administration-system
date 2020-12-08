7f80ac5a8470db7ec27e9bd7cd16346ed13e0130
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-08 15:16:58 +0700

    #13 - Add UserRoleName Entity

---
+++ src/main/java/com/hendisantika/schooladministrationsystem/entity/UserRoleName.java
@@ -0,0 +1,17 @@
+package com.hendisantika.schooladministrationsystem.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : school-administration-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 08/12/20
+ * Time: 15.16
+ */
+public enum UserRoleName {
+    ROLE_STUDENT,
+    ROLE_TEACHER,
+    ROLE_HEADTEACHER,
+    ROLE_ADMIN
+}
