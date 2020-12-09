91895ae2584d835eebae0cba692545c2f09fcb5c
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-10 05:44:40 +0700

    #38 - Add RoomResponseDTO

---
+++ src/main/java/com/hendisantika/schooladministrationsystem/dto/response/RoomResponseDTO.java
@@ -0,0 +1,21 @@
+package com.hendisantika.schooladministrationsystem.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : school-administration-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/12/20
+ * Time: 05.44
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RoomResponseDTO {
+    private String classroomNumber;
+}
