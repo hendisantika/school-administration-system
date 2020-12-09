de5444e8322363ae301839e87d135b8c6412a2ea
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-09 08:12:45 +0700

    #25 - Add CustomException

---
+++ src/main/java/com/hendisantika/schooladministrationsystem/exception/CustomException.java
@@ -0,0 +1,35 @@
+package com.hendisantika.schooladministrationsystem.exception;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : school-administration-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 09/12/20
+ * Time: 08.12
+ */
+public class CustomException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    public CustomException(String message, HttpStatus httpStatus) {
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public HttpStatus getHttpStatus() {
+        return httpStatus;
+    }
+
+}
