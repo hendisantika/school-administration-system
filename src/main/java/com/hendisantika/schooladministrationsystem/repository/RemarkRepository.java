91be6c5cf45802a556a6a76a359c76e64ce3c2db
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-10 06:03:54 +0700

    #50 - Add RemarkRepository

---
+++ src/main/java/com/hendisantika/schooladministrationsystem/repository/RemarkRepository.java
@@ -0,0 +1,18 @@
+package com.hendisantika.schooladministrationsystem.repository;
+
+import com.hendisantika.schooladministrationsystem.entity.Remark;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : school-administration-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/12/20
+ * Time: 06.03
+ */
+@Repository
+public interface RemarkRepository extends JpaRepository<Remark, Long> {
+}
