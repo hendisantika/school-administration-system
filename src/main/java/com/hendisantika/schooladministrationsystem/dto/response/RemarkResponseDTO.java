30af766f01aef8c3142a57aed8fdd048573ec13c
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-10 05:43:10 +0700

    #36 - Add RemarkResponseDTO

---
+++ src/main/java/com/hendisantika/schooladministrationsystem/dto/response/RemarkResponseDTO.java
@@ -0,0 +1,16 @@
+package com.hendisantika.schooladministrationsystem.dto.response;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : school-administration-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/12/20
+ * Time: 05.42
+ */
+public class RemarkResponseDTO {
+    private String text;
+
+    private Long studentId;
+}
