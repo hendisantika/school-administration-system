ea3044540d6daf0c8a1123126dc29e74808db197
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-10 05:41:48 +0700

    #34 - Add CourseResponseDTO

---
+++ src/main/java/com/hendisantika/schooladministrationsystem/dto/response/CourseResponseDTO.java
@@ -0,0 +1,25 @@
+package com.hendisantika.schooladministrationsystem.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : school-administration-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 10/12/20
+ * Time: 05.40
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CourseResponseDTO {
+    private String title;
+
+    private int year;
+
+    private Long teacherId;
+}
