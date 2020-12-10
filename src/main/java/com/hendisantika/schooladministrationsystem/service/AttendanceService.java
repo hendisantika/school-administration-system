5fb79f2c1fc8e9a19b58b15b3c8bfca255528d3c
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-11 05:46:35 +0700

    #71 - Add makeAttendanceFormToClassroom in AttendanceService

--- src/main/java/com/hendisantika/schooladministrationsystem/service/AttendanceService.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/service/AttendanceService.java
@@ -1,9 +1,14 @@
 package com.hendisantika.schooladministrationsystem.service;
 
+import com.hendisantika.schooladministrationsystem.dto.AttendanceDTO;
+import com.hendisantika.schooladministrationsystem.entity.user.group.Student;
 import com.hendisantika.schooladministrationsystem.repository.AttendanceRepository;
 import com.hendisantika.schooladministrationsystem.repository.user.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +26,19 @@ public class AttendanceService {
 
     @Autowired
     private StudentRepository studentRepository;
+
+    /**
+     * Returns a form that contains a list of students
+     * and boolean field for each student.
+     *
+     * @param classroom_id Id of the classroom.
+     * @return A form table to collect the missing students.
+     */
+    public List<AttendanceDTO> makeAttendanceFormToClassroom(Long classroom_id) {
+        List<AttendanceDTO> result = new ArrayList<>();
+        for (Student student : getAllStudentByClassroom(classroom_id)) {
+            result.add(new AttendanceDTO(student));
+        }
+        return result;
+    }
 }
