d280975fffc8bd655ecd1e7b77b836f60af61619
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-11 05:50:12 +0700

    #78 - Add ClassroomService

---
+++ src/main/java/com/hendisantika/schooladministrationsystem/service/ClassroomService.java
@@ -0,0 +1,32 @@
+package com.hendisantika.schooladministrationsystem.service;
+
+import com.hendisantika.schooladministrationsystem.repository.ClassroomRepository;
+import com.hendisantika.schooladministrationsystem.repository.CourseRepository;
+import com.hendisantika.schooladministrationsystem.repository.user.StudentRepository;
+import com.hendisantika.schooladministrationsystem.repository.user.TeacherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : school-administration-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 11/12/20
+ * Time: 05.49
+ */
+@Service
+public class ClassroomService {
+    @Autowired
+    TeacherRepository teacherRepository;
+    @Autowired
+    private ClassroomRepository classroomRepository;
+    @Autowired
+    private StudentRepository studentRepository;
+    @Autowired
+    private CourseRepository courseRepository;
+    @Autowired
+    private AuthorityService authService;
+
+}
