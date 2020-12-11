6b631bb2a345d634051e1344c481d9c27f4f7f3e
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-12 06:10:04 +0700

    #109 - Add findFailedStudentsInClass in HeadTeacherService

--- src/main/java/com/hendisantika/schooladministrationsystem/service/HeadTeacherService.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/service/HeadTeacherService.java
@@ -1,9 +1,15 @@
 package com.hendisantika.schooladministrationsystem.service;
 
+import com.hendisantika.schooladministrationsystem.dto.FailedStudentDTO;
+import com.hendisantika.schooladministrationsystem.entity.Course;
+import com.hendisantika.schooladministrationsystem.entity.user.group.Student;
 import com.hendisantika.schooladministrationsystem.repository.CourseRepository;
 import com.hendisantika.schooladministrationsystem.repository.user.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +28,32 @@ public class HeadTeacherService {
     @Autowired
     private CourseRepository courseRepository;
 
+    /**
+     * Find all student who failed at least one subject.
+     *
+     * @param classroomId Id of the class.
+     * @return List of Failed Student with courses.
+     */
+    public List<FailedStudentDTO> findFailedStudentsInClass(Long classroomId) {
+        List<FailedStudentDTO> result = new ArrayList<>();
+        List<Student> students = getStudentsFromClassroom(classroomId);
+        for (Student student : students) {
+            List<Course> failedCourses = new ArrayList<>();
+            for (Course course : courseRepository.findAll()) {
+                if (student.getCourses().contains(course)) {
+                    double avg = calcAverageByStudent(student, course.getId());
+                    if (avg < 2 && avg >= 1) {
+                        failedCourses.add(course);
+                    }
+                }
+            }
+            if (!failedCourses.isEmpty()) {
+                result.add(new FailedStudentDTO(
+                        student,
+                        failedCourses
+                ));
+            }
+        }
+        return result;
+    }
 }
