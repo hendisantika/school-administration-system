43b4bdf91052afa693a53edbacebe8b5307e45e9
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-12 06:01:21 +0700

    #98 - Add findAllByStudent in ExamService

--- src/main/java/com/hendisantika/schooladministrationsystem/service/ExamService.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/service/ExamService.java
@@ -1,10 +1,14 @@
 package com.hendisantika.schooladministrationsystem.service;
 
+import com.hendisantika.schooladministrationsystem.entity.Exam;
 import com.hendisantika.schooladministrationsystem.repository.CourseRepository;
 import com.hendisantika.schooladministrationsystem.repository.ExamRepository;
 import com.hendisantika.schooladministrationsystem.repository.user.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +29,19 @@ public class ExamService {
 
     @Autowired
     private CourseRepository courseRepository;
+
+    /**
+     * Returns a List of Exams that written by student on the course.
+     *
+     * @param studentId Id of the Student.
+     * @param courseId  Id of the Course.
+     * @return List of the exams.
+     */
+    public List<Exam> findAllByStudent(Long studentId, Long courseId) {
+        return examRepository.findAll()
+                .stream()
+                .filter(exam -> exam.getCourse().getId().equals(courseId))
+                .filter(exam -> exam.getStudent().getId().equals(studentId))
+                .collect(Collectors.toList());
+    }
 }
