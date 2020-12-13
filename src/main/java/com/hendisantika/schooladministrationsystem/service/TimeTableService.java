a54e3fde9396ae9f112fe22ee2d64b857f7e1246
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-14 06:07:34 +0700

    #155 - Add getTimeTableByStudent in TimeTableService

--- src/main/java/com/hendisantika/schooladministrationsystem/service/TimeTableService.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/service/TimeTableService.java
@@ -1,5 +1,7 @@
 package com.hendisantika.schooladministrationsystem.service;
 
+import com.hendisantika.schooladministrationsystem.entity.TimeTableEntity;
+import com.hendisantika.schooladministrationsystem.entity.user.group.Student;
 import com.hendisantika.schooladministrationsystem.repository.ClassroomRepository;
 import com.hendisantika.schooladministrationsystem.repository.CourseRepository;
 import com.hendisantika.schooladministrationsystem.repository.RoomRepository;
@@ -8,6 +10,8 @@ import com.hendisantika.schooladministrationsystem.repository.user.StudentReposi
 import com.hendisantika.schooladministrationsystem.repository.user.TeacherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -38,4 +42,21 @@ public class TimeTableService {
     @Autowired
     private RoomRepository roomRepository;
 
+    /**
+     * Returns a TimeTable 2d array to student. The table size
+     * is 12x5. The y diagonal represents the days and x diagonal
+     * shows the lecture.
+     *
+     * @param id Id of the Student.
+     * @return a TimeTable 2d matrix.
+     */
+    public TimeTableEntity[][] getTimeTableByStudent(Long id) {
+        TimeTableEntity[][] result = new TimeTableEntity[12][5];
+        Student student = studentRepository.getOne(id);
+        List<TimeTableEntity> timeTableEntities = getLessonsByStudent(student.getId());
+        for (TimeTableEntity tableEntity : timeTableEntities) {
+            result[tableEntity.getLessonNumber()][tableEntity.getDay()] = tableEntity;
+        }
+        return result;
+    }
 }
