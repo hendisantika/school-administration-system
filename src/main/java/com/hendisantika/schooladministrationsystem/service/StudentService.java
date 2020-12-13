e4629e2cdec20a74092c549ecd0d57f21e24b77c
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-14 05:56:27 +0700

    #131 - Add StudentService

---
+++ src/main/java/com/hendisantika/schooladministrationsystem/service/StudentService.java
@@ -0,0 +1,48 @@
+package com.hendisantika.schooladministrationsystem.service;
+
+import com.hendisantika.schooladministrationsystem.repository.AttendanceRepository;
+import com.hendisantika.schooladministrationsystem.repository.ClassroomRepository;
+import com.hendisantika.schooladministrationsystem.repository.CourseRepository;
+import com.hendisantika.schooladministrationsystem.repository.ExamRepository;
+import com.hendisantika.schooladministrationsystem.repository.ReportRepository;
+import com.hendisantika.schooladministrationsystem.repository.TeacherPreferenceRepository;
+import com.hendisantika.schooladministrationsystem.repository.user.StudentRepository;
+import com.hendisantika.schooladministrationsystem.repository.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : school-administration-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 14/12/20
+ * Time: 05.55
+ */
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ClassroomRepository classroomRepository;
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Autowired
+    private ExamRepository examRepository;
+
+    @Autowired
+    private ReportRepository reportRepository;
+
+    @Autowired
+    private AttendanceRepository attendanceRepository;
+
+    @Autowired
+    private TeacherPreferenceRepository teacherPreferenceRepository;
+}
