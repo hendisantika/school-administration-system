84c19c9fa9387c0fb866a7ebcc9746058b48a062
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-13 17:33:16 +0700

    #121 - Add findAllByStudent in RemarkService

--- src/main/java/com/hendisantika/schooladministrationsystem/service/RemarkService.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/service/RemarkService.java
@@ -1,9 +1,13 @@
 package com.hendisantika.schooladministrationsystem.service;
 
+import com.hendisantika.schooladministrationsystem.entity.Remark;
 import com.hendisantika.schooladministrationsystem.repository.RemarkRepository;
 import com.hendisantika.schooladministrationsystem.repository.user.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +26,17 @@ public class RemarkService {
 
     @Autowired
     private StudentRepository studentRepository;
+
+    /**
+     * Returns a List of Remark by student id.
+     *
+     * @param student_id
+     * @return Remarks from database by student id.
+     */
+    public List<Remark> findAllByStudent(Long student_id) {
+        return remarkRepository.findAll()
+                .stream()
+                .filter(remark -> remark.getStudent().getId().equals(student_id))
+                .collect(Collectors.toList());
+    }
 }
