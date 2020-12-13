f35dc85ef51381d2ad637ff0a17b68206bc77891
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-14 06:01:51 +0700

    #145 - Add findAll in TeacherService

--- src/main/java/com/hendisantika/schooladministrationsystem/service/TeacherService.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/service/TeacherService.java
@@ -1,11 +1,14 @@
 package com.hendisantika.schooladministrationsystem.service;
 
+import com.hendisantika.schooladministrationsystem.entity.user.group.Teacher;
 import com.hendisantika.schooladministrationsystem.repository.CourseRepository;
 import com.hendisantika.schooladministrationsystem.repository.TeacherPreferenceRepository;
 import com.hendisantika.schooladministrationsystem.repository.user.TeacherRepository;
 import com.hendisantika.schooladministrationsystem.repository.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -33,4 +36,12 @@ public class TeacherService {
     @Autowired
     private AuthorityService authService;
 
+    /**
+     * Returns a List of Teachers.
+     *
+     * @return teachers from database.
+     */
+    public List<Teacher> findAll() {
+        return teacherRepository.findAll();
+    }
 }
