ecadd788689658d5c937fb4036bf62e7c212fe4d
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-15 05:40:30 +0700

    #168 - Add findById in AuthorityService

--- src/main/java/com/hendisantika/schooladministrationsystem/service/auth/AuthorityService.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/service/auth/AuthorityService.java
@@ -1,9 +1,13 @@
 package com.hendisantika.schooladministrationsystem.service.auth;
 
+import com.hendisantika.schooladministrationsystem.entity.user.Authority;
 import com.hendisantika.schooladministrationsystem.entity.user.UserRoleName;
 import com.hendisantika.schooladministrationsystem.repository.user.AuthorityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +25,13 @@ public class AuthorityService {
 
     public void save(UserRoleName userRoleName) {
         authorityRepository.saveAuth(userRoleName.toString());
+    }
+
+    public List<Authority> findById(Long id) {
+        Authority authority = authorityRepository
+                .findById(id).orElse(null);
+        List<Authority> authorities = new ArrayList<>();
+        authorities.add(authority);
+        return authorities;
     }
 }
