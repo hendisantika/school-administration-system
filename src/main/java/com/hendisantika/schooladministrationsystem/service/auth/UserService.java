5065ced5e6a8e578b6484f7176ebdb148e3221f4
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-15 05:45:15 +0700

    #175 - Add save in UserService

--- src/main/java/com/hendisantika/schooladministrationsystem/service/auth/UserService.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/service/auth/UserService.java
@@ -1,9 +1,16 @@
 package com.hendisantika.schooladministrationsystem.service.auth;
 
+import com.hendisantika.schooladministrationsystem.dto.response.UserResponseDTO;
+import com.hendisantika.schooladministrationsystem.entity.user.Authority;
+import com.hendisantika.schooladministrationsystem.entity.user.User;
+import com.hendisantika.schooladministrationsystem.exception.CustomException;
 import com.hendisantika.schooladministrationsystem.repository.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +31,19 @@ public class UserService {
 
     @Autowired
     private AuthorityService authService;
+
+    public User save(UserResponseDTO userResponseDTO) {
+        if (!userRepository.existsByUsername(userResponseDTO.getUsername())) {
+            User newUser = new User();
+            newUser.setUsername(userResponseDTO.getUsername());
+            newUser.setPassword(passwordEncoder.encode(userResponseDTO.getPassword()));
+            newUser.setFullName(userResponseDTO.getFullName());
+            List<Authority> authorities = authService.findByName(userResponseDTO.getRole());
+            newUser.setAuthorities(authorities);
+            userRepository.save(newUser);
+            return newUser;
+        } else {
+            throw new CustomException("Username is already in use", HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+    }
 }
