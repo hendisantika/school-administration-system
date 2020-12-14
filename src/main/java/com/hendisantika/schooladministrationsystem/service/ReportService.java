8c7182b25831b8efce17aa412afe12aab0dd717d
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-15 05:50:41 +0700

    #184 - Add getSemesterResultByStudent in ReportService

--- src/main/java/com/hendisantika/schooladministrationsystem/service/ReportService.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/service/ReportService.java
@@ -1,10 +1,14 @@
 package com.hendisantika.schooladministrationsystem.service;
 
+import com.hendisantika.schooladministrationsystem.entity.Report;
 import com.hendisantika.schooladministrationsystem.repository.CourseRepository;
 import com.hendisantika.schooladministrationsystem.repository.ReportRepository;
 import com.hendisantika.schooladministrationsystem.repository.user.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,4 +30,22 @@ public class ReportService {
     @Autowired
     private CourseRepository courseRepository;
 
+    /**
+     * Returns a Summary of semester for student. One
+     * Report object is a course-mark pair. Each report made by
+     * Teacher.
+     *
+     * @param studentId Id of the student.
+     * @param year      The year when semester was.
+     * @param semester  Semester when report made.
+     * @return List of the reports.
+     */
+    public List<Report> getSemesterResultByStudent(Long studentId, int year, int semester) {
+        return reportRepository.findAll()
+                .stream()
+                .filter(report -> report.getStudent().getId().equals(studentId)
+                        && report.getYear() == year
+                        && report.getSemester() == semester)
+                .collect(Collectors.toList());
+    }
 }
