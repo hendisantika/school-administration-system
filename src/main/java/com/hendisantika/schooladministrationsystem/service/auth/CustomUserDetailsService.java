952d73a79c2efb3dd3f6f23359456eb5ff5cc9d7
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-15 05:44:07 +0700

    #173 - Add changePassword in UserDetailsService

--- src/main/java/com/hendisantika/schooladministrationsystem/service/auth/CustomUserDetailsService.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/service/auth/CustomUserDetailsService.java
@@ -4,7 +4,11 @@ import com.hendisantika.schooladministrationsystem.entity.user.User;
 import com.hendisantika.schooladministrationsystem.repository.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -19,7 +23,7 @@ import org.springframework.stereotype.Service;
  * Time: 05.41
  */
 @Service
-public class UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
 
@@ -29,6 +33,7 @@ public class UserDetailsService {
     @Autowired
     private AuthenticationManager authenticationManager;
 
+    @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(s);
         if (user == null) {
@@ -36,5 +41,21 @@ public class UserDetailsService {
         } else {
             return user;
         }
+    }
+
+    public void changePassword(String oldPassword, String newPassword) {
+
+        Authentication currentUser = SecurityContextHolder.getContext().getAuthentication();
+        String username = currentUser.getName();
+
+        if (authenticationManager != null) {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, oldPassword));
+        } else {
+            return;
+        }
+        User user = (User) loadUserByUsername(username);
+        user.setPassword(passwordEncoder.encode(newPassword));
+        userRepository.save(user);
+
     }
 }
