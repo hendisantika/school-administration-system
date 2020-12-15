d29ce59452a2a3e7be969778b9e780a529712441
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-16 05:33:08 +0700

    #228 - Add onLogoutSuccess in LogoutSuccess

--- src/main/java/com/hendisantika/schooladministrationsystem/security/auth/LogoutSuccess.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/security/auth/LogoutSuccess.java
@@ -2,8 +2,15 @@ package com.hendisantika.schooladministrationsystem.security.auth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +26,18 @@ public class LogoutSuccess implements LogoutSuccessHandler {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Override
+    public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                                Authentication authentication) {
+        Map<String, String> result = new HashMap<>();
+        result.put("result", "success");
+        httpServletResponse.setContentType("application/json");
+        try {
+            httpServletResponse.getWriter().write(objectMapper.writeValueAsString(result));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
+    }
 }
