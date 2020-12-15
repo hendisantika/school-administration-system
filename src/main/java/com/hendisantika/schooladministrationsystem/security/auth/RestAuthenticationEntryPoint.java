946987401fa4caf9ffc944973e3411c4f7b92b2d
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-16 05:33:34 +0700

    #229 - Add RestAuthenticationEntryPoint

---
+++ src/main/java/com/hendisantika/schooladministrationsystem/security/auth/RestAuthenticationEntryPoint.java
@@ -0,0 +1,29 @@
+package com.hendisantika.schooladministrationsystem.security.auth;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : school-administration-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/12/20
+ * Time: 05.33
+ */
+@Component
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                         AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+    }
+}
