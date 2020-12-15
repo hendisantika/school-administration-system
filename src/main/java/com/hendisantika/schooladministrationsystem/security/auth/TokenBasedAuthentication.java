5a1608bbccadccc1ed734bac2e39885ba04668fe
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-16 05:35:43 +0700

    #234 - Add TokenBasedAuthentication in TokenAuthenticationFilter

---
+++ src/main/java/com/hendisantika/schooladministrationsystem/security/auth/TokenBasedAuthentication.java
@@ -0,0 +1,47 @@
+package com.hendisantika.schooladministrationsystem.security.auth;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : school-administration-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/12/20
+ * Time: 05.35
+ */
+public class TokenBasedAuthentication extends AbstractAuthenticationToken {
+
+    private final UserDetails principle;
+    private String token;
+
+    public TokenBasedAuthentication(UserDetails principle) {
+        super(principle.getAuthorities());
+        this.principle = principle;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return true;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return token;
+    }
+
+    @Override
+    public UserDetails getPrincipal() {
+        return principle;
+    }
+}
