856691fc34f331b31433a334cdd194de7739140d
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-16 05:34:44 +0700

    #232 - Add doFilterInternal in TokenAuthenticationFilter

--- src/main/java/com/hendisantika/schooladministrationsystem/security/auth/TokenAuthenticationFilter.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/security/auth/TokenAuthenticationFilter.java
@@ -2,9 +2,16 @@ package com.hendisantika.schooladministrationsystem.security.auth;
 
 import com.hendisantika.schooladministrationsystem.security.TokenHelper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -36,4 +43,23 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
             LOGIN_MATCHER,
             LOGOUT_MATCHER
     );
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String authToken = tokenHelper.getToken(httpServletRequest);
+        if (authToken != null && !skipPathRequest(httpServletRequest, pathsToSkip)) {
+            try {
+                String username = tokenHelper.getUsernameFromToken(authToken);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                TokenBasedAuthentication authentication = new TokenBasedAuthentication(userDetails);
+                authentication.setToken(authToken);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } catch (Exception e) {
+                SecurityContextHolder.getContext().setAuthentication(new AnonAuthentication());
+            }
+        }
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+    }
 }
