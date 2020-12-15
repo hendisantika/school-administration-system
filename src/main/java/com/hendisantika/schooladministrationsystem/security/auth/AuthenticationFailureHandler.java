aa4bd864c981e1f80d8dfccf40c38b145a6e1410
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-16 05:31:09 +0700

    #224 - Add AuthenticationFailureHandler

---
+++ src/main/java/com/hendisantika/schooladministrationsystem/security/auth/AuthenticationFailureHandler.java
@@ -0,0 +1,29 @@
+package com.hendisantika.schooladministrationsystem.security.auth;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : school-administration-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 16/12/20
+ * Time: 05.30
+ */
+@Component
+public class AuthenticationFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+                                        AuthenticationException exception) throws IOException, ServletException {
+        super.onAuthenticationFailure(request, response, exception);
+    }
+}
