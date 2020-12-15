5d1f49886ecf074bceff4ca3fd249d98f0016a2d
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-16 05:32:25 +0700

    #226 - Add onAuthenticationSuccess in AuthenticationSuccessHandler

--- src/main/java/com/hendisantika/schooladministrationsystem/security/auth/AuthenticationSuccessHandler.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/security/auth/AuthenticationSuccessHandler.java
@@ -1,11 +1,20 @@
 package com.hendisantika.schooladministrationsystem.security.auth;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hendisantika.schooladministrationsystem.entity.user.User;
+import com.hendisantika.schooladministrationsystem.entity.user.UserTokenState;
 import com.hendisantika.schooladministrationsystem.security.TokenHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,4 +39,24 @@ public class AuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccess
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        clearAuthenticationAttributes(request);
+        User user = (User) authentication.getPrincipal();
+
+        String jws = tokenHelper.generateToken(user.getUsername());
+
+        Cookie authCookie = new Cookie(TOKEN_COOKIE, (jws));
+        authCookie.setHttpOnly(true);
+        authCookie.setMaxAge(EXPIRES_IN);
+        authCookie.setPath("/");
+        response.addCookie(authCookie);
+
+        UserTokenState userTokenState = new UserTokenState(jws, EXPIRES_IN);
+        String jwtResponse = objectMapper.writeValueAsString(userTokenState);
+        response.setContentType("application/json");
+        response.getWriter().write(jwtResponse);
+    }
 }
