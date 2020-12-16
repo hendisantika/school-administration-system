dc9b457bf0f32ff7b2af87b58c64f4bad81c9653
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-17 05:40:24 +0700

    #238 - Add passwordEncoder in WebSecurityConfig

--- src/main/java/com/hendisantika/schooladministrationsystem/config/WebSecurityConfig.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/config/WebSecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
@@ -56,5 +58,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new TokenAuthenticationFilter();
     }
 
-
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
