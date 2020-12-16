1e7b1f4ecac9bc2509b3abcbfa25d08c4730fbb9
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-17 05:40:40 +0700

    #239 - Add configureGlobal in WebSecurityConfig

--- src/main/java/com/hendisantika/schooladministrationsystem/config/WebSecurityConfig.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -61,5 +62,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder authenticationManagerBuilder)
+            throws Exception {
+        authenticationManagerBuilder.userDetailsService(jwtUserDetailsService)
+                .passwordEncoder(passwordEncoder());
+
     }
 }
