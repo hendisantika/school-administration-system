63fbca417b0f9b3dd5d14f7914ded71600b2ae20
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-17 05:50:53 +0700

    #251 - Add verify API in AttendanceController

--- src/main/java/com/hendisantika/schooladministrationsystem/controller/AttendanceController.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/AttendanceController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -70,6 +71,19 @@ public class AttendanceController {
     @DeleteMapping(value = "/attendances/{id}")
     public String delete(@PathVariable Long id) {
         attendanceService.delete(id);
+        return id.toString();
+    }
+
+    @PreAuthorize("hasRole('ROLE_TEACHER') or hasRole('ROLE_HEADTEACHER')")
+    @ApiOperation(value = "${AttendanceController.verify}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Attendance doesn't found"),
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+    @PutMapping(value = "/attendances/verify/{id}")
+    public String verify(@PathVariable Long id) {
+        attendanceService.verify(id);
         return id.toString();
     }
 }
