c0c9b568ebbb9c70d991bbc95ded54858fbec7e3
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-17 05:39:37 +0700

    #235 - Add WebSecurityConfig

---
+++ src/main/java/com/hendisantika/schooladministrationsystem/config/WebSecurityConfig.java
@@ -0,0 +1,45 @@
+package com.hendisantika.schooladministrationsystem.config;
+
+import com.hendisantika.schooladministrationsystem.security.auth.LogoutSuccess;
+import com.hendisantika.schooladministrationsystem.security.auth.RestAuthenticationEntryPoint;
+import com.hendisantika.schooladministrationsystem.service.auth.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : school-administration-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 17/12/20
+ * Time: 05.38
+ */
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${jwt.cookie}")
+    private String TOKEN_COOKIE;
+
+    @Autowired
+    private CustomUserDetailsService jwtUserDetailsService;
+
+    @Autowired
+    private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+
+    @Autowired
+    private LogoutSuccess logoutSuccess;
+
+    @Autowired
+    private AuthenticationSuccessHandler authenticationSuccessHandler;
+
+    @Autowired
+    private AuthenticationFailureHandler authenticationFailureHandler;
+
+}
