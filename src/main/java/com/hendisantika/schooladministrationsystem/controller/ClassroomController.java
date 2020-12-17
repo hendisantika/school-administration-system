20d7b3a97d004be72bccf9ca2d46fba99110283f
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-18 06:21:48 +0700

    #264 - Add update API in ClassroomController

--- src/main/java/com/hendisantika/schooladministrationsystem/controller/ClassroomController.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/ClassroomController.java
@@ -12,6 +12,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -80,5 +81,18 @@ public class ClassroomController {
     @PostMapping(value = "/classrooms/create")
     public Classroom create(@RequestBody ClassroomResponseDTO classroomResponseDTO) {
         return classroomService.create(classroomResponseDTO);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "${ClassroomController.update}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Classroom doesn't found"),
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+    @PutMapping(value = "/classrooms/update/{id}")
+    public Classroom update(@PathVariable Long id,
+                            @RequestBody ClassroomResponseDTO classroomResponseDTO) {
+        return classroomService.update(id, classroomResponseDTO);
     }
 }
