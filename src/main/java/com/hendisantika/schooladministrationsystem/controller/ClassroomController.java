e18f4dd95878b367bf106b51b7e4e5fa8be8636e
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-18 06:20:08 +0700

    #260 - Add findAll API in ClassroomController

--- src/main/java/com/hendisantika/schooladministrationsystem/controller/ClassroomController.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/ClassroomController.java
@@ -1,10 +1,17 @@
 package com.hendisantika.schooladministrationsystem.controller;
 
+import com.hendisantika.schooladministrationsystem.entity.Classroom;
 import com.hendisantika.schooladministrationsystem.service.ClassroomService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +29,14 @@ public class ClassroomController {
     @Autowired
     private ClassroomService classroomService;
 
+    @ApiOperation(value = "${ClassroomController.findAll}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Classrooms don't found"),
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+    @GetMapping(value = "/classrooms/all")
+    public List<Classroom> findAll() {
+        return classroomService.findAll();
+    }
 }
