c885afeca09821267a58634dca6f3be45aa38218
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-18 06:31:47 +0700

    #282 - Add update API in ExamController

--- src/main/java/com/hendisantika/schooladministrationsystem/controller/ExamController.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/ExamController.java
@@ -12,6 +12,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -69,5 +70,18 @@ public class ExamController {
     @PostMapping(value = "/exams/create")
     public Exam create(@RequestBody ExamResponseDTO examResponseDTO) {
         return examService.create(examResponseDTO);
+    }
+
+    @PreAuthorize("hasRole('ROLE_TEACHER') or hasRole('ROLE_HEADTEACHER')")
+    @ApiOperation(value = "${ExamController.update}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Exam doesn't found"),
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+    @PutMapping(value = "/exams/update/{id}")
+    public Exam update(@PathVariable Long id,
+                       @RequestBody ExamResponseDTO examResponseDTO) {
+        return examService.update(id, examResponseDTO);
     }
 }
