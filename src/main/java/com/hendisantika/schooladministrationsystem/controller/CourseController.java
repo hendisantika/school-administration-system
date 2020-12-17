cde3f111a0793a310dc9396102cdfaf1a162c0fe
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-18 06:25:26 +0700

    #269 - Add CourseController

---
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/CourseController.java
@@ -0,0 +1,24 @@
+package com.hendisantika.schooladministrationsystem.controller;
+
+import com.hendisantika.schooladministrationsystem.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : school-administration-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 18/12/20
+ * Time: 06.24
+ */
+@RestController
+@RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
+public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+}
