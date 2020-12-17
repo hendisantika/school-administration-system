8ae8e52c06174ebdce0341db8fc47bbe0804cab9
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-18 06:27:39 +0700

    #273 - Add create API in CourseController

--- src/main/java/com/hendisantika/schooladministrationsystem/controller/CourseController.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/CourseController.java
@@ -1,5 +1,6 @@
 package com.hendisantika.schooladministrationsystem.controller;
 
+import com.hendisantika.schooladministrationsystem.dto.response.CourseResponseDTO;
 import com.hendisantika.schooladministrationsystem.entity.Course;
 import com.hendisantika.schooladministrationsystem.service.CourseService;
 import io.swagger.annotations.ApiOperation;
@@ -7,8 +8,11 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -61,5 +65,17 @@ public class CourseController {
     @GetMapping(value = "/courses/teacher/{teacherId}")
     public List<Course> getCoursesByTeacherId(@PathVariable Long teacherId) {
         return courseService.getCoursesByTeacherId(teacherId);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "${CourseController.create}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Course cannot created"),
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+    @PostMapping(value = "/courses/create")
+    public Course create(@RequestBody CourseResponseDTO courseResponseDTO) {
+        return courseService.create(courseResponseDTO);
     }
 }
