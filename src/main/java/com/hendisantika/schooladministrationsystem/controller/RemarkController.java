3d4924ff939d8eb93ddc4313aa9f489a00b408e2
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-19 05:47:22 +0700

    #297 - Add findAllByStudent API in RemarkController

--- src/main/java/com/hendisantika/schooladministrationsystem/controller/RemarkController.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/RemarkController.java
@@ -1,10 +1,19 @@
 package com.hendisantika.schooladministrationsystem.controller;
 
+import com.hendisantika.schooladministrationsystem.entity.Remark;
 import com.hendisantika.schooladministrationsystem.service.RemarkService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +30,17 @@ public class RemarkController {
 
     @Autowired
     private RemarkService remarkService;
+
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_TEACHER') or hasRole('ROLE_HEADTEACHER')" +
+            "or @securityService.hasStudentAccess(principal.id, #studentId)")
+    @ApiOperation(value = "${RemarkController.findAllByStudent}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Remarks don't found"),
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+    @GetMapping(value = "/remarks/student/{studentId}")
+    public List<Remark> findAllByStudent(@PathVariable Long studentId) {
+        return remarkService.findAllByStudent(studentId);
+    }
 }
