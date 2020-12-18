0929fe5ca559f1d0f8dc2f423310494562fd30a9
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-19 05:51:10 +0700

    #307 - Add update API in ReportController

--- src/main/java/com/hendisantika/schooladministrationsystem/controller/ReportController.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/ReportController.java
@@ -12,6 +12,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -70,5 +71,18 @@ public class ReportController {
     @PostMapping(value = "/reports/create")
     public Report create(@RequestBody ReportResponseDTO reportResponseDTO) {
         return reportService.create(reportResponseDTO);
+    }
+
+    @PreAuthorize("hasRole('ROLE_TEACHER') or hasRole('ROLE_HEADTEACHER')")
+    @ApiOperation(value = "${ReportController.update}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Report doesn't found"),
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+    @PutMapping(value = "/reports/update/{id}")
+    public Report update(@PathVariable Long id,
+                         @RequestBody ReportResponseDTO reportResponseDTO) {
+        return reportService.update(id, reportResponseDTO);
     }
 }
