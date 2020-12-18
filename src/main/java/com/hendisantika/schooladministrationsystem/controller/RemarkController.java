03526e3ce57997b1f199ffba50194fbf03d19946
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-19 05:48:19 +0700

    #300 - Add update API in RemarkController

--- src/main/java/com/hendisantika/schooladministrationsystem/controller/RemarkController.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/RemarkController.java
@@ -12,6 +12,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -70,5 +71,18 @@ public class RemarkController {
     @PostMapping(value = "/remarks/create")
     public Remark create(@RequestBody RemarkResponseDTO remarkResponseDTO) {
         return remarkService.create(remarkResponseDTO);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_TEACHER') or hasRole('ROLE_HEADTEACHER')")
+    @ApiOperation(value = "${RemarkController.update}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Remark doesn't found"),
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+    @PutMapping(value = "/remarks/update/{id}")
+    public Remark update(@PathVariable Long id,
+                         @RequestBody RemarkResponseDTO remarkResponseDTO) {
+        return remarkService.update(id, remarkResponseDTO);
     }
 }
