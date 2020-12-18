9f95983df1f2bc18d0a46542df3a4fcaafd2b28f
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-19 05:40:32 +0700

    #287 - Add HeadTeacherController

---
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/HeadTeacherController.java
@@ -0,0 +1,24 @@
+package com.hendisantika.schooladministrationsystem.controller;
+
+import com.hendisantika.schooladministrationsystem.service.HeadTeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : school-administration-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 19/12/20
+ * Time: 05.40
+ */
+@RestController
+@RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
+public class HeadTeacherController {
+
+    @Autowired
+    private HeadTeacherService headTeacherService;
+}
