2a13ca572647256e861638f933deca0e9583c07d
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-19 05:50:19 +0700

    #304 - Add getSemesterResultByStudent API in ReportController

--- src/main/java/com/hendisantika/schooladministrationsystem/controller/ReportController.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/ReportController.java
@@ -1,10 +1,20 @@
 package com.hendisantika.schooladministrationsystem.controller;
 
+import com.hendisantika.schooladministrationsystem.entity.Report;
 import com.hendisantika.schooladministrationsystem.service.ReportService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +31,18 @@ public class ReportController {
 
     @Autowired
     private ReportService reportService;
+
+    @PreAuthorize("hasRole('ROLE_TEACHER') or hasRole('ROLE_HEADTEACHER')" +
+            "or @securityService.hasStudentAccess(principal.id, #studentId)")
+    @ApiOperation(value = "${ReportController.getSemesterResultByStudent}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Semester doesn't found"),
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+    @PostMapping(value = "/reports/{studentId}/{year}")
+    public List<Report> getSemesterResultByStudent(@PathVariable Long studentId,
+                                                   @PathVariable int year, @RequestBody int semester) {
+        return reportService.getSemesterResultByStudent(studentId, year, semester);
+    }
 }
