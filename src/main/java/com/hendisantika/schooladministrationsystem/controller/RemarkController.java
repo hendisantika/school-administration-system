bc88a136e66b6075dfaf1e84c4afaaa1bf059ebb
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-19 05:49:10 +0700

    #302 - Add delete API in RemarkController

--- src/main/java/com/hendisantika/schooladministrationsystem/controller/RemarkController.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/RemarkController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -84,5 +85,18 @@ public class RemarkController {
     public Remark update(@PathVariable Long id,
                          @RequestBody RemarkResponseDTO remarkResponseDTO) {
         return remarkService.update(id, remarkResponseDTO);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_TEACHER') or hasRole('ROLE_HEADTEACHER')")
+    @ApiOperation(value = "${RemarkController.delete}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Remark doesn't found"),
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+    @DeleteMapping(value = "/remarks/{id}")
+    public String delete(@PathVariable Long id) {
+        remarkService.delete(id);
+        return id.toString();
     }
 }
