ba58f8cfa08e192451053a5e7aaccbbdc6161ea7
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-19 05:44:30 +0700

    #291 - Add findAll API in MessageController

--- src/main/java/com/hendisantika/schooladministrationsystem/controller/MessageController.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/MessageController.java
@@ -1,10 +1,17 @@
 package com.hendisantika.schooladministrationsystem.controller;
 
+import com.hendisantika.schooladministrationsystem.entity.Message;
 import com.hendisantika.schooladministrationsystem.service.MessageService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +28,15 @@ public class MessageController {
 
     @Autowired
     private MessageService messageService;
+
+    @ApiOperation(value = "${MessageController.findAll}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Messages don't found"),
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+    @GetMapping(value = "/messages/all")
+    public List<Message> findAll() {
+        return messageService.findAll();
+    }
 }
