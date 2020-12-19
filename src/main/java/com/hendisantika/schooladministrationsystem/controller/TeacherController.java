ebbd549592491e54648630c93bea6c52a77e862a
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-20 05:33:43 +0700

    #326 - Add findById API in TeacherController

--- src/main/java/com/hendisantika/schooladministrationsystem/controller/TeacherController.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/TeacherController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +41,16 @@ public class TeacherController {
     @GetMapping(value = "/teachers/all")
     public List<Teacher> findAll() {
         return teacherService.findAll();
+    }
+
+    @ApiOperation(value = "${TeacherController.findById}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Teacher doesn't found"),
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+    @GetMapping(value = "/teachers/{id}")
+    public Teacher findById(@PathVariable Long id) {
+        return teacherService.findById(id);
     }
 }
