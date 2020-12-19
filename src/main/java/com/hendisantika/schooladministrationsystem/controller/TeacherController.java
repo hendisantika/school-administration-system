1222e0cd88e2013f235be7610309618d11a48eca
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-20 05:34:20 +0700

    #328 - Add create API in TeacherController

--- src/main/java/com/hendisantika/schooladministrationsystem/controller/TeacherController.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/TeacherController.java
@@ -1,5 +1,6 @@
 package com.hendisantika.schooladministrationsystem.controller;
 
+import com.hendisantika.schooladministrationsystem.dto.response.TeacherResponseDTO;
 import com.hendisantika.schooladministrationsystem.entity.user.group.Teacher;
 import com.hendisantika.schooladministrationsystem.service.TeacherService;
 import io.swagger.annotations.ApiOperation;
@@ -10,6 +11,8 @@ import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -63,5 +66,17 @@ public class TeacherController {
     @GetMapping(value = "/teachers/user/{userId}")
     public Teacher findByUserId(@PathVariable Long userId) {
         return teacherService.findByUserId(userId);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "${TeacherController.create}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Teacher cannot created"),
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+    @PostMapping(value = "/teachers/create")
+    public Teacher create(@RequestBody TeacherResponseDTO teacherResponseDTO) {
+        return teacherService.create(teacherResponseDTO);
     }
 }
