bd1b71e59e38fbaa2302585273481984c3b46fb2
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-20 05:26:04 +0700

    #313 - Add findById API in RoomController

--- src/main/java/com/hendisantika/schooladministrationsystem/controller/RoomController.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/RoomController.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,5 +39,16 @@ public class RoomController {
     @GetMapping(value = "/rooms/all")
     public List<Room> findAll() {
         return roomService.findAll();
+    }
+
+    @ApiOperation(value = "${RoomController.FindById}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Room doesn't found"),
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+    @GetMapping(value = "/rooms/{id}")
+    public Room findById(@PathVariable Long id) {
+        return roomService.findById(id);
     }
 }
