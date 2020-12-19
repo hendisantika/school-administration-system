8ec7f9dd0ec9e0b414580928bbc145dde6f8ac00
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-20 05:31:38 +0700

    #322 - Add delete API in StudentController

--- src/main/java/com/hendisantika/schooladministrationsystem/controller/StudentController.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/StudentController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -104,5 +105,18 @@ public class StudentController {
     public Student update(@PathVariable Long id,
                           @RequestBody StudentResponseDTO studentResponseDTO) {
         return studentService.update(id, studentResponseDTO);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "${StudentController.delete}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Student doesn't found"),
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+    @DeleteMapping(value = "/students/{id}")
+    public String delete(@PathVariable Long id) {
+        studentService.delete(id);
+        return id.toString();
     }
 }
