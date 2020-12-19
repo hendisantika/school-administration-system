3bf1f791b5fd78dfbc005b3d31ff9874e21ea9f4
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-20 05:29:47 +0700

    #317 - Add findAll API in StudentController

--- src/main/java/com/hendisantika/schooladministrationsystem/controller/StudentController.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/StudentController.java
@@ -1,10 +1,18 @@
 package com.hendisantika.schooladministrationsystem.controller;
 
+import com.hendisantika.schooladministrationsystem.entity.user.group.Student;
 import com.hendisantika.schooladministrationsystem.service.StudentService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +29,17 @@ public class StudentController {
 
     @Autowired
     private StudentService studentService;
+
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_TEACHER') or hasRole('ROLE_HEADTEACHER')")
+    @ApiOperation(value = "${StudentController.findAll}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Students don't found"),
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+    @GetMapping(value = "/students/all")
+    public List<Student> findAll() {
+        return studentService.findAll();
+    }
+
 }
