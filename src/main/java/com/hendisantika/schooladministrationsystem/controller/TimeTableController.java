c02b68f87506b89522bc45aa30413fd1f0041c64
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-21 09:24:36 +0700

    #341 - Add delete API in TimeTableController

--- src/main/java/com/hendisantika/schooladministrationsystem/controller/TimeTableController.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/TimeTableController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -105,5 +106,18 @@ public class TimeTableController {
     public TimeTableEntity update(@PathVariable Long id,
                                   @RequestBody TimeTableEntityResponseDTO timeTableEntityResponseDTO) {
         return timeTableService.update(id, timeTableEntityResponseDTO);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "${TimeTableController.delete}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "TimetableEntity doesn't found"),
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+    @DeleteMapping(value = "/timetables/{id}")
+    public String delete(@PathVariable Long id) {
+        timeTableService.delete(id);
+        return id.toString();
     }
 }
