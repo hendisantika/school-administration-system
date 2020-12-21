3573517759ad4cb3e6e2f19bd8809616e79e3361
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-22 06:25:48 +0700

    #359 - Add classroom.ts model

---
+++ school-webapp/src/app/model/classroom.ts
@@ -0,0 +1,11 @@
+import {Teacher} from './teacher';
+
+export class Classroom {
+
+  id: number;
+  start_year: number;
+  end_year: number;
+  year: number;
+  letter: string;
+  headTeacher: Teacher;
+}
