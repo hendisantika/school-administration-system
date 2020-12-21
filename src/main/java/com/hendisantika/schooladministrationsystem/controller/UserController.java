c3e62e8a981d8aef362af82557994486da087bfa
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-21 09:26:15 +0700

    #343 - Add getAll API in UserController

--- src/main/java/com/hendisantika/schooladministrationsystem/controller/UserController.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/UserController.java
@@ -1,10 +1,18 @@
 package com.hendisantika.schooladministrationsystem.controller;
 
+import com.hendisantika.schooladministrationsystem.entity.user.User;
 import com.hendisantika.schooladministrationsystem.service.auth.UserService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,4 +30,15 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @ApiOperation(value = "Get all Users.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "The users don't exists"),
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+    @GetMapping(value = "/user/all")
+    public List<User> getAll() {
+        return userService.findAll();
+    }
 }
