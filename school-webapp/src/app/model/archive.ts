257ca6ef2a0a8d260ef5220697cb462541526b31
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-22 06:22:46 +0700

    #356 - Add archive.ts model

---
+++ school-webapp/src/app/model/archive.ts
@@ -0,0 +1,10 @@
+import {ArchiveReport} from './archiveReport';
+
+export class Archive {
+
+  id: number;
+  username: string;
+  studentName: string;
+  dateOfBirth: string;
+  reports: ArchiveReport[];
+}
