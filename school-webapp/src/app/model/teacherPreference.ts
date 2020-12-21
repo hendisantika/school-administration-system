246edb8cc5cd4f08ac4b89337c1bf92d9b5354a6
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-22 06:33:41 +0700

    #368 - Add teacherPreference.ts model

---
+++ school-webapp/src/app/model/teacherPreference.ts
@@ -0,0 +1,11 @@
+import {Teacher} from './teacher';
+
+export class TeacherPreference {
+
+  id: number;
+  teacher: Teacher;
+  testWeight: number;
+  topicTestWeight: number;
+  repetitionWeight: number;
+  homeworkWeight: number;
+}
