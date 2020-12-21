e7646c0300a7a6d01d0c2fb46777a001a361513b
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-21 09:25:38 +0700

    #342 - Add UserController

---
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.hendisantika.schooladministrationsystem.controller;
+
+import com.hendisantika.schooladministrationsystem.service.auth.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : school-administration-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 21/12/20
+ * Time: 09.25
+ */
+@RestController
+@RequestMapping(value = "/api", produces = MediaType.APPLICATION_JSON_VALUE)
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+}
