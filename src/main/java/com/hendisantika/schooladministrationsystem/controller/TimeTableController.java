272513b0433bd8f61b9f2103347c527b6c0161d5
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-21 09:18:58 +0700

    #335 - Add getTimeTableByStudent API in TimeTableController

--- src/main/java/com/hendisantika/schooladministrationsystem/controller/TimeTableController.java
+++ src/main/java/com/hendisantika/schooladministrationsystem/controller/TimeTableController.java
@@ -1,8 +1,15 @@
 package com.hendisantika.schooladministrationsystem.controller;
 
+import com.hendisantika.schooladministrationsystem.entity.TimeTableEntity;
 import com.hendisantika.schooladministrationsystem.service.TimeTableService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +28,16 @@ public class TimeTableController {
 
     @Autowired
     private TimeTableService timeTableService;
+
+    @PreAuthorize("hasRole('ROLE_ADMIN') or @securityService.hasStudentAccess(principal.id, #id)")
+    @ApiOperation(value = "${TimeTableController.getTimeTableByTeacher}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Something went wrong"),
+            @ApiResponse(code = 403, message = "Access denied"),
+            @ApiResponse(code = 404, message = "Timetable doesn't found"),
+            @ApiResponse(code = 500, message = "Expired or invalid JWT token")})
+    @GetMapping(value = "/timetables/student/{id}")
+    public TimeTableEntity[][] getTimeTableByStudent(@PathVariable Long id) {
+        return timeTableService.getTimeTableByStudent(id);
+    }
 }
