7ebf3100bd94b7ed728b735a89a3306868dff6e2
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-23 08:30:10 +0700

    #387 - Add studentResponseDTO.ts

---
+++ school-webapp/src/app/dto/response/studentResponseDTO.ts
@@ -0,0 +1,15 @@
+export class StudentResponseDTO {
+
+  username: string;
+  dateOfBirth: string;
+  start_year: number;
+  address: string;
+  gender: string;
+  educationId: string;
+  healthCareId: string;
+  parent1Name: string;
+  parent2Name: string;
+  parent1Phone: string;
+  parent2Phone: string;
+  classroom_id: number;
+}
