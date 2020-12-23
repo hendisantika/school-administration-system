5bb1f09750ec495cff27327af44cef64495c53d1
Hendi Santika <hendisantika@yahoo.co.id>
2020-12-23 08:26:54 +0700

    #377 - Add reportDTO.ts

---
+++ school-webapp/src/app/dto/reportDTO.ts
@@ -0,0 +1,8 @@
+import {Student} from '../model/student';
+import {ReportResponseDTO} from './response/reportResponseDTO';
+
+export class ReportDTO {
+
+  student: Student;
+  reportResponseDTO: ReportResponseDTO;
+}
